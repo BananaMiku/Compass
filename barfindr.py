e39a42b477d806583d8dbbeee19e3a23e31e537e
Austin Henlotter <ahenlotter@umass.edu>
2026-02-21 14:18:01 -0500

    fixed some stuff

--- barfindr.py
+++ barfindr.py
@@ -1,6 +1,7 @@
 import requests
 import config
 import keys
+import location_calculations as lc
 
 def get_bars(cur_location, radius):
     if config.TEST == True:
@@ -16,27 +17,29 @@ def get_bars(cur_location, radius):
     return data
 
 def find_closest_bar(bars, cur_location):
-    if bars.status != 200 or 'results' not in bars:
+    if 'results' not in bars:
         print("malformed api res 🍺")
         return None
     cur_closest = None
     dist = float('inf')
     for bar in bars['results']:
-        bar_cords = extract_lat_long(bar)
-        bar_dist = get_distance(cur_location, bar_cords) 
+        bar_cords = extract_lat_lng(bar)
+        bar_dist = lc.get_distance(cur_location, bar_cords) 
         if bar_dist <= dist:
             cur_closest = bar_cords
             dist = bar_dist
+    print(dist)
     return cur_closest
         
 def extract_lat_lng(google_res):
     if 'geometry' not in google_res or 'location' not in google_res['geometry']:
         return none
-    return [google_res['geometry']['location']['lat'], google_res['geometry']['location']['lng']]
+    return google_res['geometry']['location']
 
 def test():
-    bars = get_bars([0, 0], 1500)
-    print(find_closest_bar(bars, [0,0] ))
+    cur_location = {'lat': 0, 'lng': 0}
+    bars = get_bars(cur_location, 1500)
+    print(find_closest_bar(bars, cur_location))
 
 if __name__ == '__main__':
     test()
