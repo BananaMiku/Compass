0a46e68b10a8932b78694f56779dc19d82814eca
Austin Henlotter <ahenlotter@umass.edu>
2026-02-21 15:58:09 -0500

    fixed incorrect closest

--- barfindr.py
+++ barfindr.py
@@ -4,6 +4,9 @@ import keys
 import location_calculations as lc
 
 def get_bars(cur_location, radius):
+    if config.TEST:
+        print("TEST MODE")
+        return config.MOCK_PLACE_DATA
     url = "https://places.googleapis.com/v1/places:searchNearby"
     headers = {
         "Content-Type": "application/json",
@@ -34,12 +37,11 @@ def find_closest_bar(bars, cur_location):
     dist = float('inf')
     for bar in bars['places']:
         bar_cords = extract_lat_lng(bar)
-        cur_cords = extract_lat_lng(cur_closest) if cur_closest != None else None
-        bar_dist = lc.get_distance(cur_cords, bar_cords) 
+        bar_dist = lc.get_distance(cur_location, bar_cords) 
         if bar_dist <= dist:
             cur_closest = bar
             dist = bar_dist
-        print(bar_dist, bar)
+        print(cur_closest, bar)
     print(dist)
     return cur_closest
         
@@ -49,7 +51,7 @@ def extract_lat_lng(google_res):
     return {'lat': google_res['location']['latitude'], 'lng': google_res['location']['longitude']}
 
 def test():
-    cur_location = {'lat': 42.3894, 'lng':  -72.5265 }
+    cur_location = {'lat': 42.2743676, 'lng': -72.6594507}
     bars = get_bars(cur_location, 16093.44)
     print(bars)
     print(find_closest_bar(bars, cur_location))
